ae24aaf094c0de076dfab37b66915c51174ca30d
wawj901124 <410287958@qq.com>
2019-04-16 17:21:05 +0800

    run_all_test中rootpath外加一级目录

--- TestCaseFunction/main/run_all_test.py
+++ TestCaseFunction/main/run_all_test.py
@@ -4,7 +4,7 @@ import unittest
 import os
 import sys
 
-rootpath = str(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+rootpath = str(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 syspath = sys.path
 sys.path = []
 sys.path.append(rootpath)  # 将工程根目录加入到python搜索路径中
