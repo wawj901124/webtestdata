5e40ed1f0274a897e368ad466786acc9ad28341e
wawj901124 <410287958@qq.com>
2019-04-17 16:58:37 +0800

    全量测试脚本一起测试

--- TestCaseFunction/test/alltest_list.py
+++ TestCaseFunction/test/alltest_list.py
@@ -8,16 +8,18 @@ from TestCaseFunction.autotest.test.agent.AutoTestDetails import TestDetailsClas
 from TestCaseFunction.autotest.test.agent.AutoTestMerchantList import TestMerchantListClass
 from TestCaseFunction.autotest.test.agent.AutoTestRevise import TestReviseClass
 from TestCaseFunction.autotest.test.manager.AutoTestReview import TestReviewClass
+from TestCaseFunction.autotest.test.agent.AutoTestMerchantListElementCheck import TestMerchantListClass
 
 def caselist():
     alltestnames = [
-        # 'TestCaseFunction.autotest.test.agent.AutoTestLogin.TestLoginClass',
-        # 'TestCaseFunction.autotest.test.agent.AutoTestAccountInfo.TestAccountInfoClass',
-        # 'TestCaseFunction.autotest.test.agent.AutoTestCheckContract.TestCheckContractClass',
-        # 'TestCaseFunction.autotest.test.agent.AutoTestDetails.TestDetailsClass',
-        # 'TestCaseFunction.autotest.test.agent.AutoTestMerchantList.TestMerchantListClass',
-        # 'TestCaseFunction.autotest.test.agent.AutoTestAddMerchant.TestAddMerchantClass',
-        # 'TestCaseFunction.autotest.test.agent.AutoTestAddCompanyMerchant.TestAddCompanyMerchantClass',
+        'TestCaseFunction.autotest.test.agent.AutoTestLogin.TestLoginClass',
+        'TestCaseFunction.autotest.test.agent.AutoTestAccountInfo.TestAccountInfoClass',
+        'TestCaseFunction.autotest.test.agent.AutoTestCheckContract.TestCheckContractClass',
+        'TestCaseFunction.autotest.test.agent.AutoTestDetails.TestDetailsClass',
+        'TestCaseFunction.autotest.test.agent.AutoTestMerchantListElementCheck.TestMerchantListClass',
+        'TestCaseFunction.autotest.test.agent.AutoTestMerchantList.TestMerchantListClass',
+        'TestCaseFunction.autotest.test.agent.AutoTestAddMerchant.TestAddMerchantClass',
+        'TestCaseFunction.autotest.test.agent.AutoTestAddCompanyMerchant.TestAddCompanyMerchantClass',
         'TestCaseFunction.autotest.test.agent.AutoTestRevise.TestReviseClass',
         'TestCaseFunction.autotest.test.manager.AutoTestReview.TestReviewClass',
 
