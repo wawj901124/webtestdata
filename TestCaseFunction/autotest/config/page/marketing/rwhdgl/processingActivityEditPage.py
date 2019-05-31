0cfad25bd3e44efe915e3005cc3bc649c5ea2a2d
wawj901124 <410287958@qq.com>
2019-05-31 17:45:54 +0800

    更正任务活动中进行中活动的页面链接

--- TestCaseFunction/autotest/config/page/marketing/rwhdgl/processingActivityEditPage.py
+++ TestCaseFunction/autotest/config/page/marketing/rwhdgl/processingActivityEditPage.py
@@ -5,7 +5,7 @@ from TestCaseFunction.util.gettimestr import GetTimeStr
 class ProcesingActivityEditPage:
     filename = "createactivityid.txt"
     editactivityid = GetTimeStr().readText(filename)
-    pageurl = "%s/nereus/marketing/admin/v/#/activityManage/missionAct/modifyOnLine/%s" % (WEB_URL_TITLE,editactivityid )
+    pageurl = "%s/nereus/marketing/admin/v/#/activityManage/missionAct/modifyOffLine/%s" % (WEB_URL_TITLE,editactivityid )
     # pageurl = "%s/nereus/marketing/admin/v/#/activityManage/missionAct/modifyOffLine/%s" % (WEB_URL_TITLE, MARKETING_EDIT_PROCESSING_ACTIVITYID)
     yxhdcj = ""
     yxhdcj_text = u"营销活动创建"
@@ -25,10 +25,7 @@ class ProcesingActivityEditPage:
     hdsj_starttime_secondtime = ""
     hdsj_starttime_queding = ""
     hdsj_endtime = "/html/body/div[1]/div/div[2]/div[2]/div/div/div[1]/div/form/div[1]/div[1]/div[2]/div/div[4]/div/div/div/div/div[1]/div/input"
-
-
     hdsj_endtime_rightmove = "/html/body/div[1]/div/div[2]/div[2]/div/div/div[1]/div/form/div[1]/div[1]/div[2]/div/div[4]/div/div/div/div/div[2]/div/div/div/div[1]/span[5]/i"
-
     hdsj_endtime_daytime = "/html/body/div[1]/div/div[2]/div[2]/div/div/div[1]/div/form/div[1]/div[1]/div[2]/div/div[4]/div/div/div/div/div[2]/div/div/div/div[2]/div/span[26]/em"
     hdsj_endtime_secondtime = ""
     hdsj_endtime_queding = "/html/body/div[1]/div/div[2]/div[2]/div/div/div[1]/div/form/div[1]/div[1]/div[2]/div/div[4]/div/div/div/div/div[2]/div/div/div/div[4]/button[3]"
