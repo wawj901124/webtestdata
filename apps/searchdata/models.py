31b8b20e281bb7bdcf12907f0c806791c09bedaf
wawj901124 <410287958@qq.com>
2019-06-18 14:03:38 +0800

    增加是否点击最有一页页码测试脚本

--- apps/searchdata/models.py
+++ apps/searchdata/models.py
@@ -6,6 +6,7 @@ class SearchData(models.Model):
     webproject = models.CharField(max_length=100, default="", verbose_name=u"web后台项目")
     testpage = models.CharField(max_length=100, default="", verbose_name=u"测试页面")
     testcasetitle = models.CharField(max_length=100, default="", verbose_name=u"测试内容的名称")
+    isclicklastpage = models.BooleanField(default=False,verbose_name=u"是否点击最后一页页码")
     selectxpath = models.CharField(max_length=1000, default="",null=True, blank=True, verbose_name=u"筛选字段选项Xpath路径")
     selectoptiontextxpath = models.CharField(max_length=1000, default="",null=True, blank=True, verbose_name=u"筛选字段选项内容Xpath路径")
     selectinputxpath = models.CharField(max_length=1000, default="",null=True, blank=True, verbose_name=u"筛选字段选项对应输入框的Xpath路径")
